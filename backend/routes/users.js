d45e9c39cf04a20b25d9b09b61d47ad069923d1d
Sean <pca.sean.jeun@gmail.com>
2023-09-30 17:53:53 -0400

    setup

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('my name jeff');
+  res.send('from users');
 });
 
 module.exports = router;
