bb4dcb0a85f4adc92fa5d40c06eef53c682b22c3
Sean <pca.sean.jeun@gmail.com>
2023-09-30 20:28:00 -0400

    public folder and routes/index.js removed/app.js reconfigured accordingly

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/api/users');
 const tweetsRouter = require('./routes/api/tweets')
 
@@ -13,9 +11,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/tweets', tweetsRouter)
 
